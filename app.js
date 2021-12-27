81b16521dfcc5a54fd201a4b1e3b656cd18b660b
J Scott Chapman <scott8675309@gmail.com>
2021-12-27 14:07:13 -0700

    Get the loads from FreightMonster

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
+const request = require('request');
 
-app.get("/", (req, res) => res.send("Hello there!"));
+console.log('Running...');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+let url = 'https://portal.freightmonster.com/api/v2/loads';
+
+request.get(
+    {
+        url: url,
+        json: true,
+        headers: { 'User-Agent': 'request' }
+    },
+    (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+        } else {
+            // console.log({ data });
+            console.log(typeof data);
+            let loadNumbers = data.map((load) => {
+                return load.load;
+            });
+            console.log({ loadNumbers });
+            console.log('count', data.length);
+        }
+    }
+);
+
+// fetchUrl(
+//     'https://portal.freightmonster.com/api/v2/loads',
+//     (error, meta, body) => {
+//         console.log({ error });
+//         console.log({ meta });
+//         console.log('body', body.toString());
+//         console.log('count', body.json());
+//     }
+// );
+console.log('Shutting down');
